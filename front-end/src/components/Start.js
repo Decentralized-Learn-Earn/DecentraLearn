4648c64ede5b3d50e08442b8d738cd3c20103b33
Dilan Tompkins <65461019+TomTerrific7@users.noreply.github.com>
2021-10-07 13:24:01 -0400

    uniswap content

--- front-end/src/components/Start.js
+++ front-end/src/components/Start.js
@@ -7,7 +7,7 @@ const Start = ({ onQuizStart }) => {
     <div className="card">
       <div className="card-content">
         <div className="content">
-          <p>Uniswap is a decentralized finance protocol that is used to exchange cryptocurrencies. Uniswap is also the name of the company that initially built the Uniswap protocol. The protocol facilitates automated transactions between cryptocurrency tokens on the Ethereum blockchain through the use of smart contracts </p>
+          <p>Uniswap is a decentralized finance protocol that is used to exchange cryptocurrencies. Uniswap is also the name of the company that initially built the Uniswap protocol. The protocol facilitates automated transactions between cryptocurrency tokens on the Ethereum blockchain through the use of smart contracts. The native token of Uniswap is the UNI Token, which allows holders of the token to vote.The Uniswap V3 core whitew paper was written in March 2021, has five authors and nine pages. Uniswap provides a large ecosystem for developers, traders, and liquidity providers. to learn more, please visit our website at uniswap.org.  </p>
           
           <Button color="primary" variant="contained" className="button is-info is-medium" onClick={onQuizStart}>Start Quiz</Button>
         </div>
@@ -16,4 +16,4 @@ const Start = ({ onQuizStart }) => {
   );
 }
 
-export default Start;
+export default Start; 
